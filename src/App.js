e533976eaa1efe8535270d78b28321778b47c9ab
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-08-23 11:59:14 +0530

    Adding another dependency sass in the project and adding imports to the root element App.js.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
+//Style
+import './sass/index.scss';
+//Components
 import Add from "./Components/Add";
 import List from "./Components/List";
+import Config from "./Components/Config";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Msg from "./Components/Msg";
+//Img
+import HeaderDarkMobile from './assests/bg-mobile-dark.jpg';
+import HeaderDarkDesktop from './assests/bg-desktop-dark.jpg';
+import HeaderLightMobile from './assests/bg-mobile-light.jpg';
+import HeaderLightDesktop from './assests/bg-desktop-light.jpg';
+//Icon
+import {BiLoaderAlt} from "react-icons/bi";
 
 const App = () => {
   return (
-    <div className="App">
-      App Component!
+    <div className="content">
+      <Header/>
       <Add/>
+      <Msg/>
       <List/>
+      <Config/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/Config.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Config = () => {
+    return(
+        <div>
+           Config Component 
+        </div>
+    )
+}
+export default Config;
