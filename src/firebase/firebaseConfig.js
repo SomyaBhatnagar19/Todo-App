fc29f1ce55a02115448ed71d5a68adf06aed59cb
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-08-23 12:59:54 +0530

    Setting up realtime database from firebase called firestore.

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: "todo-app-6292a",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const store = getFirestore(app);
+export default store;
